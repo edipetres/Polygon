bf95f99c77acb4aa1f4f7e92d35641d37fffd519
Minerva Suvanto <minerva.suvanto@gmail.com>
2016-03-29 13:56:29 +0200

    Customer class

---
+++ src/java/Domain/Customer.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Domain;
+
+/**
+ *
+ * @author Minerva
+ */
+public class Customer {
+    int customer_id;
+    String company_name;
+    String fname;
+    String lname;
+    String username;
+    String pwd;
+    String email;
+    String phone_no;
+    
+    
+}
