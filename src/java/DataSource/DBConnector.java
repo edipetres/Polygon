def34ec2c8936a2a76cac4efb6d628965242e5a3
edipetres <edipetres@10.20.146.16>
2016-03-29 12:16:06 +0200

    database connection setup

--- src/java/DataSource/DBConnector.java
+++ src/java/DataSource/DBConnector.java
@@ -1,14 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package DataSource;
 
-/**
- *
- * @author edipetres
- */
-public class DBConnector {
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+// Encapsulates code to connect to DB
+// Implemented as a Singleton to ensure max one connection
+public class DBConnector
+{
+
+    private static String driver = "com.mysql.jdbc.Driver";
+    private static String URL = "jdbc:mysql://localhost:3306/CHANGEDIR";
+    private static String id = "root";			
+    private static String pw = "";
     
+    private Connection con;
+
+    //-- Singleton ---- 
+    private static DBConnector instance;
+    private DBConnector()
+    {
+        try
+        {
+            Class.forName(driver);
+            con = DriverManager.getConnection(URL, id, pw);   // The connection will be released upon program 
+		  					      // termination	
+        } catch (Exception e)
+        {
+            System.out.println("\n*** Remember to insert your Oracle ID and PW in the DBConnector class! ***\n");
+            System.out.println("error in DBConnector.getConnection()");
+            System.out.println(e);
+        }
+    }
+    
+    public static DBConnector getInstance()
+    {
+        if (instance == null)
+            instance = new DBConnector();
+        return instance;
+    }
+    //------------------
+    
+    public Connection getConnection()
+    {
+      return con;
+    }
 }
--- src/java/DataSource/DBFacade.java
+++ src/java/DataSource/DBFacade.java
@@ -5,10 +5,30 @@
  */
 package DataSource;
 
+import java.sql.Connection;
+
 /**
  *
  * @author edipetres
  */
 public class DBFacade {
     
+    private Connection con;
+    //add mappers here as private objects
+    
+    //Singleton start
+    private static DBFacade instance;
+    
+    private DBFacade () {
+        con = DBConnector.getInstance().getConnection();
+        //assign the mappers with a new object here
+    }
+    
+    public static DBFacade getInstance() {
+        if (instance == null) {
+            instance = new DBFacade();
+        }
+        return instance;
+    }
+    //Singleton end
 }
